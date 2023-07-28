a5c4d35ce4f99944a27bb5c060b142772082abe4
Sachin Rao <91653541+sachinrao24@users.noreply.github.com>
2023-07-28 11:46:36 +0530

    Add files via upload

--- pages/2_Documentation.py
+++ pages/2_Documentation.py
@@ -2,10 +2,10 @@ import streamlit as st
 
 
 st.title('Entity terminology:')
-st.write('Symptoms- Refers to any symptoms of a disease identified in the text, such as “fever”, “cough”, etc.')
-st.write('Diseases- Refers to the diseases identified in the text, such as “malaria”, “dengue”, etc.')
-st.write('Locations- Refers to the geographic locations, names of cities, states, etc. identified in the text such as, “Lucknow”, “Jalandhar”, etc.')
-st.write('Numeric values- Refers to numeric entities identified in the text which can be numbers, percentages, or qualitative descriptors such as “increasing”, “many”, etc.')
+st.write('*Symptoms*- Refers to any symptoms of a disease identified in the text, such as “fever”, “cough”, etc.')
+st.write('*Diseases*- Refers to the diseases identified in the text, such as “malaria”, “dengue”, etc.')
+st.write('*Locations*- Refers to the geographic locations, names of cities, states, etc. identified in the text such as, “Lucknow”, “Jalandhar”, etc.')
+st.write('*Numeric values*- Refers to numeric entities identified in the text which can be numbers, percentages, or qualitative descriptors such as “increasing”, “many”, etc.')
 st.caption('**Note:** In the web application, all entity types are preceded with the word, “Potential” since although utmost care has been taken to ensure accuracy of the results, it is important to note that the results are computer-generated estimations and may not always be completely precise.')
 
 st.divider()
